dd9e68a81eb4a2830d3301e38a7a2cd7617925be
Tasfiq97 <tasfiqulazad200@gmail.com>
2021-10-30 22:01:51 +0600

    another-commit

--- index.js
+++ index.js
@@ -1,10 +1,68 @@
 const express = require('express')
 const app = express()
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
 const cors=require("cors")
-const port = 5000
-
+const ObjectId=require("mongodb").ObjectId;
+const port =process.env.PORT || 5000
+// added middlewire 
 app.use(cors());
  app.use(express.json())
+
+ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qbrq9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run(){
+  try{
+    await client.connect();
+      
+    const database = client.db("deliveryDb");
+    const offerCollection = database.collection("offers");
+const usersCollection=database.collection("users")
+  
+    // get method of offfers
+
+    app.get("/offers",async(req,res)=>{
+     
+      const result=await offerCollection.find({}).toArray();
+      res.json(result);
+    })
+
+    // get method 
+
+    app.get("/offers/:id",async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:ObjectId(id)};
+      const result=await offerCollection.findOne(query);
+    res.json(result)
+
+    })
+// post method 
+
+app.post("/orderInfo",async(req,res)=>{
+  const result=await usersCollection.insertOne(req.body);
+  res.json(result);
+});
+
+// get method user 
+
+app.get("/manageOrder",async(req,res)=>{
+
+  const result=await usersCollection.find({}).toArray();
+  res.json(result)
+})
+
+  }
+  finally{
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
