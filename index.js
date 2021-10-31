005679d9976103f764af209a8e96bb92cd415371
Tasfiq97 <tasfiqulazad200@gmail.com>
2021-10-31 15:33:53 +0600

    another one

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 
  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qbrq9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
+console.log(uri)
 
 async function run(){
   try{
@@ -38,7 +38,7 @@ const usersCollection=database.collection("users")
     res.json(result)
 
     })
-// post method 
+// post method user
 
 app.post("/orderInfo",async(req,res)=>{
   const result=await usersCollection.insertOne(req.body);
@@ -53,6 +53,39 @@ app.get("/manageOrder",async(req,res)=>{
   res.json(result)
 })
 
+// delete method 
+
+app.delete("/deleteOrder/:id",async(req,res)=>{
+    const id=req.params.id;
+    console.log(id)
+    const query={_id:ObjectId(id)};
+    console.log(query);
+    const result= await usersCollection.deleteOne(query);
+    console.log(result);
+     res.json(result);
+})
+  // post method offers 
+  app.post("/offers",async(req,res)=>{
+    const query=req.body;
+    const result=await offerCollection.insertOne(query)
+    res.json(result);
+  })
+
+  // update method status 
+
+  app.put("/update/:id",async(req,res)=>{
+    id=req.params.id;
+    const update=req.body;
+    const filter={_id:ObjectId(id)};
+    const updateBody={
+      $set:{
+        status:"approved"
+      }
+    }
+    const result= await usersCollection.updateOne(filter,updateBody);
+    // console.log(result,req.body)
+    res.json(result);
+  })
   }
   finally{
     // await client.close();
